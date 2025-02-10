59cf886f519ea7b757bcd5af2ea6cf28b70b2860
VictorHugo-7 <victorhotpinho7@gmail.com>
2025-02-10 09:47:07 -0300

    Criação menu e organização das pastas

---
+++ src/js/menu.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const temaBotao = document.getElementById("my-menu-navegacaoTema");
+    const body = document.body;
+
+    // Verifica se o usuário já escolheu um tema antes
+    if (localStorage.getItem("tema") === "dark") {
+        body.classList.add("dark-mode");
+        temaBotao.classList.replace("fa-sun", "fa-moon");
+    }
+
+    temaBotao.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+
+        // Alternar ícone entre sol e lua
+        if (body.classList.contains("dark-mode")) {
+            temaBotao.classList.replace("fa-sun", "fa-moon");
+            localStorage.setItem("tema", "dark");
+        } else {
+            temaBotao.classList.replace("fa-moon", "fa-sun");
+            localStorage.setItem("tema", "light");
+        }
+    });
+});
+
