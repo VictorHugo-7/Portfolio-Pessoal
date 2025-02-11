86361b5b78f9065015e23877ffe66d379d61b565
VictorHugo-7 <victorhotpinho7@gmail.com>
2025-02-11 12:27:33 -0300

    Criação página contato

---
+++ src/js/contato.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('contactForm').addEventListener('submit', function (event) {
+        event.preventDefault(); // Impede o comportamento padrão do formulário
+
+        const loadingOverlay = document.getElementById('my-s1Contato-loadingOverlay');
+        loadingOverlay.classList.add('active'); // Exibir overlay de carregamento
+
+        const formData = new FormData(this);
+
+        fetch('https://formsubmit.co/victorhotpinho7@gmail.com', {
+            method: 'POST',
+            body: formData,
+        })
+            .then(response => {
+                loadingOverlay.classList.remove('active'); // Esconder overlay ao receber resposta
+
+                if (response.ok) {
+                    alert('Mensagem enviada com sucesso!');
+                    document.getElementById('contactForm').reset(); // Resetar formulário
+                } else {
+                    return response.text().then(text => { throw new Error(text); });
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao enviar o formulário:', error);
+                alert('Houve um problema ao enviar sua mensagem. Tente novamente mais tarde.');
+            })
+            .finally(() => {
+                loadingOverlay.classList.remove('active'); // Garantir que o overlay seja removido
+            });
+    });
+});
