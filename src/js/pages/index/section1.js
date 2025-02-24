75b5882d535b73b50a08b371f9a269ca15ca226d
VictorHugo-7 <victorhotpinho7@gmail.com>
2025-02-23 22:07:42 -0300

    atualização

--- src/js/pages/index/section1.js
+++
@@ -1,8 +0,0 @@
-fetch('../../../../src/html/pages/index/section1.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('my-index-s1-importacao').innerHTML = data;
-
-
-    })
-    .catch(error => console.error('Erro ao carregar a página:', error));
