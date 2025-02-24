87bf3297fd33ddbfa5439054323fa24cdf4b9e97
VictorHugo-7 <victorhotpinho7@gmail.com>
2025-02-23 22:01:42 -0300

    teste script

---
+++ src/js/pages/index/section1.js
@@ -0,0 +1,8 @@
+fetch('../../../../src/html/pages/index/section1.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('my-index-s1-importacao').innerHTML = data;
+
+
+    })
+    .catch(error => console.error('Erro ao carregar a página:', error));
