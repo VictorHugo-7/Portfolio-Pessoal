6b73052b12577c3c7ff221cee82b6ebda703931d
VictorHugo-7 <victorhotpinho7@gmail.com>
2025-03-04 10:39:06 -0300

    teste

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,24 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const temaBotao = document.getElementById("my-menu-navegacaoTema");
-    const body = document.body;
+fetch('../../src/html/components/menu.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('my-menu-importacao').innerHTML = data;
 
-    // Verifica se o usuário já escolheu um tema antes
-    if (localStorage.getItem("tema") === "dark") {
-        body.classList.add("dark-mode");
-        temaBotao.classList.replace("fa-sun", "fa-moon");
-    }
+        // Script Menu
+        document.addEventListener("DOMContentLoaded", function () {
 
-    temaBotao.addEventListener("click", function () {
-        body.classList.toggle("dark-mode");
+            /* Pegar os valores */
+            const temaBotao = document.getElementById("my-menu-navegacaoTema");
+            const body = document.body;
+        
+            // Verifica se o usuário já escolheu um tema antes
+            if (localStorage.getItem("tema") === "dark") {
+                body.classList.add("dark-mode");
+                temaBotao.classList.replace("fa-sun", "fa-moon");
+            }
+        
+            temaBotao.addEventListener("click", function () {
+                body.classList.toggle("dark-mode");
+        
+                // Alternar ícone entre sol e lua
+                if (body.classList.contains("dark-mode")) {
+                    temaBotao.classList.replace("fa-sun", "fa-moon");
+                    localStorage.setItem("tema", "dark");
+                } else {
+                    temaBotao.classList.replace("fa-moon", "fa-sun");
+                    localStorage.setItem("tema", "light");
+                }
+            });
+        });
 
-        // Alternar ícone entre sol e lua
-        if (body.classList.contains("dark-mode")) {
-            temaBotao.classList.replace("fa-sun", "fa-moon");
-            localStorage.setItem("tema", "dark");
-        } else {
-            temaBotao.classList.replace("fa-moon", "fa-sun");
-            localStorage.setItem("tema", "light");
-        }
-    });
-});
-
+    })
+    .catch(error => console.error('Erro ao carregar a página:', error));
