efe04d589aa0ec3226e8ea70c4e8b616242cab5c
VICTOR HUGO GAETANO OLIVEIRA TAVARES DE PINHO <24.00947-4@maua.br>
2025-03-06 08:53:58 -0300

    atualização tema

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,25 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    /* Pegar os valores */
     const temaBotao = document.getElementById("my-menu-navegacaoTema");
     const body = document.body;
 
-    // Verifica se o usuário já escolheu um tema antes
-    if (localStorage.getItem("tema") === "dark") {
-        body.classList.add("dark-mode");
-        temaBotao.classList.replace("fa-sun", "fa-moon");
-    }
+    // Verifica se o tema escuro está ativo
+    const isDarkMode = localStorage.getItem("tema") === "dark";
+    body.classList.toggle("dark-mode", isDarkMode);
+    temaBotao.classList.toggle("fa-moon", isDarkMode);
+    temaBotao.classList.toggle("fa-sun", !isDarkMode);
 
     temaBotao.addEventListener("click", function () {
-        body.classList.toggle("dark-mode");
-
-        // Alternar ícone entre sol e lua
-        if (body.classList.contains("dark-mode")) {
-            temaBotao.classList.replace("fa-sun", "fa-moon");
-            localStorage.setItem("tema", "dark");
-        } else {
-            temaBotao.classList.replace("fa-moon", "fa-sun");
-            localStorage.setItem("tema", "light");
-        }
+        const isDarkMode = body.classList.toggle("dark-mode");
+        temaBotao.classList.toggle("fa-moon", isDarkMode);
+        temaBotao.classList.toggle("fa-sun", !isDarkMode);
+        localStorage.setItem("tema", isDarkMode ? "dark" : "light");
     });
 });
