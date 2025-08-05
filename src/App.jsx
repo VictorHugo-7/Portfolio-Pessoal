bc0794166ae823be4115eb9f1c14d1c94b75deee
VictorHugo-7 <victorhotpinho7@gmail.com>
2025-08-05 20:28:54 -0300

    feat: implement dark mode toggle managed through props in App.jsx and NavBar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-/* import SpacingVertical from "./components/SpacingVertical"; */
+import { useEffect, useState } from "react";
+import SpacingVertical from "./components/SpacingVertical";
 import NavBar from "./components/NavBar";
-/* import Footer from "./components/Footer"; */
+import Footer from "./components/Footer";
+import S1_Sobre from "./components/sections/S1_Sobre";
 
 
 function App() {
+    const [isDark, setIsDark] = useState(false);
+
+    useEffect(() => {
+        if (isDark)
+            document.documentElement.classList.add('dark');
+        else
+            document.documentElement.classList.remove('dark');
+    }, [isDark]);
+
+
     return (
         <div>
-            <NavBar />
-            {/* <div id="sobre">
+            <NavBar isDark={isDark} setIsDark={setIsDark} />
+            <div id="sobre">
                 <SpacingVertical size={100.67} />
-                SOBRE <br />
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus deleniti nostrum ipsa id ullam repudiandae ab itaque aut? Labore commodi at cumque ullam neque, provident eaque aliquid quidem rem quae!
-                Mollitia eveniet vero quasi delectus magnam sit fuga, dicta sunt? Voluptates nemo adipisci voluptas vitae accusantium amet, atque minima quisquam repudiandae odio. Eligendi odio facere, culpa unde dolore magnam ipsa.
-                Nihil doloremque culpa eum assumenda ducimus mollitia nobis sint quo odit recusandae tempora totam laudantium laboriosam eveniet deserunt, id illo officiis eaque error harum voluptas labore consequatur, reiciendis illum? Possimus.
-                Mollitia natus repellat ea voluptas ex dicta dolor sapiente quo? Qui accusamus illum, odit officia quia iste aliquid, dolorem quibusdam soluta vitae harum laudantium. Animi blanditiis laboriosam at pariatur dolores.
-                Neque, accusantium? Possimus, culpa nisi? Blanditiis excepturi officiis, commodi ipsam eos neque est consequuntur quibusdam molestiae aperiam ut quia rerum fuga, nemo nulla. Consequatur, laudantium in? Sed provident sunt fugit?
-                Nostrum assumenda repellat, illum dolorem porro quis, veritatis rem voluptas ullam inventore perspiciatis pariatur eum perferendis, eveniet tempora sapiente. Sequi cupiditate cumque perspiciatis quis asperiores id vero nihil eligendi magnam!
-                Esse modi, veritatis autem sequi delectus accusamus temporibus totam. Autem accusamus exercitationem quasi iste neque numquam ab ipsam dolor vero! Debitis nulla recusandae laborum odio quisquam ipsum exercitationem omnis quaerat.
-                Obcaecati quod, accusantium odio officia ea eius nulla veritatis tempora libero quae corporis repellendus maxime, at necessitatibus officiis a nesciunt fugit in debitis? Enim illo aut labore numquam? Asperiores, impedit!
-                Doloribus, consequatur! Modi impedit recusandae labore, ipsam temporibus dolorem provident at veniam perferendis asperiores quidem aspernatur reiciendis animi. Natus labore, dignissimos suscipit ipsum quae inventore fugiat perferendis sint ipsam similique.
-                Voluptatibus ex, ducimus maiores ullam dolor autem voluptates, asperiores tenetur perspiciatis eaque facilis magnam quod tempora quo deleniti reprehenderit id eos laborum magni totam eveniet soluta commodi ut. Accusantium, sed.
+                <S1_Sobre isDark={isDark} />
             </div>
 
-            <div id="habilidades">
+            {/* <div id="habilidades">
                 <SpacingVertical size={50.67} />
                 HABILIDADES <br />
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus deleniti nostrum ipsa id ullam repudiandae ab itaque aut? Labore commodi at cumque ullam neque, provident eaque aliquid quidem rem quae!
@@ -81,6 +83,8 @@ function App() {
                 Doloribus, consequatur! Modi impedit recusandae labore, ipsam temporibus dolorem provident at veniam perferendis asperiores quidem aspernatur reiciendis animi. Natus labore, dignissimos suscipit ipsum quae inventore fugiat perferendis sint ipsam similique.
                 Voluptatibus ex, ducimus maiores ullam dolor autem voluptates, asperiores tenetur perspiciatis eaque facilis magnam quod tempora quo deleniti reprehenderit id eos laborum magni totam eveniet soluta commodi ut. Accusantium, sed.
             </div> */}
+            <SpacingVertical size={50.67} />
+            <Footer />
         </div>
     );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Sun, Moon, Menu, X } from "lucide-react";
 import logo_light from "../assets/NavBar/logo_light.svg";
 import logo_dark from "../assets/NavBar/logo_dark.svg";
 
-function NavBar() {
-    const [isDark, setIsDark] = useState(false);
+function NavBar({ isDark, setIsDark }) {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    useEffect(() => {
-        if (isDark)
-            document.documentElement.classList.add('dark');
-        else
-            document.documentElement.classList.remove('dark');
-    }, [isDark]);
 
     const toggleDarkMode = () => { setIsDark(!isDark); };
     const toggleMenu = () => { setIsMenuOpen(!isMenuOpen); };
@@ -35,25 +27,27 @@ function NavBar() {
 
                 <button
                     onClick={toggleDarkMode}
-                    className="p-1 flex items-center gap-2 border-2 border-[var(--color-secondary)] rounded-md cursor-pointer hover:bg-[var(--color-secondary)]">
-                    {isDark ? <Sun size={20} className="fill-[var(--color-primary)]" /> : <Moon size={20} className="fill-[var(--color-primary)]" />}
+                    className="p-1 flex items-center gap-2 border-2 border-[var(--color-secondary)] rounded-md cursor-pointer hover:bg-[var(--color-secondary)] transition-all">
+                    {isDark
+                        ? <Sun size={20} className="fill-[var(--color-primary)]" />
+                        : <Moon size={20} className="fill-[var(--color-primary)]" />}
                     {isDark ? 'Claro' : 'Escuro'}
                 </button>
             </div>
 
             {/* Menu Mobile */}
             <div className="md:hidden flex items-center gap-6">
-                <button
-                    onClick={toggleDarkMode}>
-                    {isDark ? <Sun size={22} className="fill-[var(--color-primary)]" /> : <Moon size={22} className="fill-[var(--color-primary)]" />}
+                <button onClick={toggleDarkMode}>
+                    {isDark
+                        ? <Sun size={22} className="fill-[var(--color-primary)]" />
+                        : <Moon size={22} className="fill-[var(--color-primary)]" />}
                 </button>
-                <button
-                    onClick={toggleMenu}>
+                <button onClick={toggleMenu}>
                     {isMenuOpen ? <X size={28} /> : <Menu size={28} />}
                 </button>
             </div>
 
-            {/* Dropdown Menu */}
+            {/* Dropdown Mobile */}
             {isMenuOpen && (
                 <div className="absolute top-12 left-0 w-full flex flex-col items-center gap-6 py-2 bg-[var(--color-background)] text-[var(--color-primary)] border-b border-[var(--color-secondary)] md:hidden z-40">
                     <a href="#sobre" onClick={toggleMenu} className="hover:text-[var(--color-highlight)]">Sobre</a>
