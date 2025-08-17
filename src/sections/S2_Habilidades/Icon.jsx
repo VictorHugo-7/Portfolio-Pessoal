297aef2165cf4875ba2a73e0c81e3e57889e405c
VictorHugo-7 <victorhotpinho7@gmail.com>
2025-08-17 00:23:39 -0300

    section habilidades estilo

--- src/sections/S2_Habilidades/Icon.jsx
+++ src/sections/S2_Habilidades/Icon.jsx
@@ -6,7 +6,7 @@ function Icon({ habilidade, isDark, animationDelay }) {
             className="w-20 h-20 relative border-[1.5px] border-[var(--color-secondary)] rounded-md transition-all duration-300 hover:-translate-y-[3px] cursor-default opacity-0 animate-fadeIn"
             style={{ animationDelay: `${animationDelay}s` }}
         >
-            <div className="absolute left-0 top-1/2 -translate-y-1/2 w-[2px] h-6 bg-[var(--color-highlight)] rounded-r-md shadow-[0_0_20px_0_rgba(59,130,246,0.5)]"></div>
+            <div className="w-[2px] h-6 absolute left-0 top-1/2 -translate-y-1/2 bg-[var(--color-highlight)] rounded-r-md shadow-[0_0_20px_0_rgba(59,130,246,0.5)]"></div>
 
             <div className="h-full flex flex-col items-center justify-center gap-2">
                 <img
