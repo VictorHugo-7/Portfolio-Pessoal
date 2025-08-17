aba22dda9ba336599761d0bb652ff1410a6fdec7
VictorHugo-7 <victorhotpinho7@gmail.com>
2025-08-17 13:51:39 -0300

    corrige paths de imagem usando modulo js na section habilidades

---
+++ src/db/S2_Habilidades/db.js
@@ -0,0 +1,82 @@
+import htmlDark from '../../assets/sections/S2_Habilidades/dark/html.svg';
+import htmlLight from '../../assets/sections/S2_Habilidades/light/html.svg';
+import cssDark from '../../assets/sections/S2_Habilidades/dark/css.svg';
+import cssLight from '../../assets/sections/S2_Habilidades/light/css.svg';
+import javascriptDark from '../../assets/sections/S2_Habilidades/dark/javascript.svg';
+import javascriptLight from '../../assets/sections/S2_Habilidades/light/javascript.svg';
+import csharpDark from '../../assets/sections/S2_Habilidades/dark/csharp.svg';
+import csharpLight from '../../assets/sections/S2_Habilidades/light/csharp.svg';
+import javaDark from '../../assets/sections/S2_Habilidades/dark/java.svg';
+import javaLight from '../../assets/sections/S2_Habilidades/light/java.svg';
+import pythonDark from '../../assets/sections/S2_Habilidades/dark/python.svg';
+import pythonLight from '../../assets/sections/S2_Habilidades/light/python.svg';
+import sqlserverDark from '../../assets/sections/S2_Habilidades/dark/sqlserver.svg';
+import sqlserverLight from '../../assets/sections/S2_Habilidades/light/sqlserver.svg';
+import mysqlDark from '../../assets/sections/S2_Habilidades/dark/mysql.svg';
+import mysqlLight from '../../assets/sections/S2_Habilidades/light/mysql.svg';
+import mongodbDark from '../../assets/sections/S2_Habilidades/dark/mongodb.svg';
+import mongodbLight from '../../assets/sections/S2_Habilidades/light/mongodb.svg';
+import nodejsDark from '../../assets/sections/S2_Habilidades/dark/nodejs.svg';
+import nodejsLight from '../../assets/sections/S2_Habilidades/light/nodejs.svg';
+import viteDark from '../../assets/sections/S2_Habilidades/dark/vite.svg';
+import viteLight from '../../assets/sections/S2_Habilidades/light/vite.svg';
+import bootstrapDark from '../../assets/sections/S2_Habilidades/dark/bootstrap.svg';
+import bootstrapLight from '../../assets/sections/S2_Habilidades/light/bootstrap.svg';
+import reactDark from '../../assets/sections/S2_Habilidades/dark/react.svg';
+import reactLight from '../../assets/sections/S2_Habilidades/light/react.svg';
+import tailwindDark from '../../assets/sections/S2_Habilidades/dark/tailwind.svg';
+import tailwindLight from '../../assets/sections/S2_Habilidades/light/tailwind.svg';
+import gitDark from '../../assets/sections/S2_Habilidades/dark/git.svg';
+import gitLight from '../../assets/sections/S2_Habilidades/light/git.svg';
+import githubDark from '../../assets/sections/S2_Habilidades/dark/github.svg';
+import githubLight from '../../assets/sections/S2_Habilidades/light/github.svg';
+import figmaDark from '../../assets/sections/S2_Habilidades/dark/figma.svg';
+import figmaLight from '../../assets/sections/S2_Habilidades/light/figma.svg';
+import azureDark from '../../assets/sections/S2_Habilidades/dark/azure.svg';
+import azureLight from '../../assets/sections/S2_Habilidades/light/azure.svg';
+import vscodeDark from '../../assets/sections/S2_Habilidades/dark/vscode.svg';
+import vscodeLight from '../../assets/sections/S2_Habilidades/light/vscode.svg';
+import notionDark from '../../assets/sections/S2_Habilidades/dark/notion.svg';
+import notionLight from '../../assets/sections/S2_Habilidades/light/notion.svg';
+import obsidianDark from '../../assets/sections/S2_Habilidades/dark/obsidian.svg';
+import obsidianLight from '../../assets/sections/S2_Habilidades/light/obsidian.svg';
+import trelloDark from '../../assets/sections/S2_Habilidades/dark/trello.svg';
+import trelloLight from '../../assets/sections/S2_Habilidades/light/trello.svg';
+import excelDark from '../../assets/sections/S2_Habilidades/dark/excel.svg';
+import excelLight from '../../assets/sections/S2_Habilidades/light/excel.svg';
+import wordDark from '../../assets/sections/S2_Habilidades/dark/word.svg';
+import wordLight from '../../assets/sections/S2_Habilidades/light/word.svg';
+import powerpointDark from '../../assets/sections/S2_Habilidades/dark/powerpoint.svg';
+import powerpointLight from '../../assets/sections/S2_Habilidades/light/powerpoint.svg';
+
+const dados = {
+  habilidades: [
+    { nome: "HTML", iconeDark: htmlDark, iconeLight: htmlLight },
+    { nome: "CSS", iconeDark: cssDark, iconeLight: cssLight },
+    { nome: "JavaScript", iconeDark: javascriptDark, iconeLight: javascriptLight },
+    { nome: "C#", iconeDark: csharpDark, iconeLight: csharpLight },
+    { nome: "Java", iconeDark: javaDark, iconeLight: javaLight },
+    { nome: "Python", iconeDark: pythonDark, iconeLight: pythonLight },
+    { nome: "SQL Server", iconeDark: sqlserverDark, iconeLight: sqlserverLight },
+    { nome: "MySQL", iconeDark: mysqlDark, iconeLight: mysqlLight },
+    { nome: "MongoDB", iconeDark: mongodbDark, iconeLight: mongodbLight },
+    { nome: "Node.js", iconeDark: nodejsDark, iconeLight: nodejsLight },
+    { nome: "Vite", iconeDark: viteDark, iconeLight: viteLight },
+    { nome: "Bootstrap", iconeDark: bootstrapDark, iconeLight: bootstrapLight },
+    { nome: "React", iconeDark: reactDark, iconeLight: reactLight },
+    { nome: "Tailwind CSS", iconeDark: tailwindDark, iconeLight: tailwindLight },
+    { nome: "Git", iconeDark: gitDark, iconeLight: gitLight },
+    { nome: "GitHub", iconeDark: githubDark, iconeLight: githubLight },
+    { nome: "Figma", iconeDark: figmaDark, iconeLight: figmaLight },
+    { nome: "Azure", iconeDark: azureDark, iconeLight: azureLight },
+    { nome: "VS Code", iconeDark: vscodeDark, iconeLight: vscodeLight },
+    { nome: "Notion", iconeDark: notionDark, iconeLight: notionLight },
+    { nome: "Obsidian", iconeDark: obsidianDark, iconeLight: obsidianLight },
+    { nome: "Trello", iconeDark: trelloDark, iconeLight: trelloLight },
+    { nome: "Excel", iconeDark: excelDark, iconeLight: excelLight },
+    { nome: "Word", iconeDark: wordDark, iconeLight: wordLight },
+    { nome: "PowerPoint", iconeDark: powerpointDark, iconeLight: powerpointLight }
+  ]
+};
+
+export default dados;
--- src/sections/S2_Habilidades/S2_Habilidades.jsx
+++ src/sections/S2_Habilidades/S2_Habilidades.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import dados from "../../db/S2_Habilidades/db.json";
+// ALTERAÇÃO 1: Importando do novo arquivo .js
+import dados from "../../db/S2_Habilidades/db.js";
 import Icon from "./Icon";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -8,8 +9,9 @@ function S2_Habilidades({ isDark }) {
     const grupoTamanho = 8;
     const habilidadesEmGrupos = [];
 
-    for (let i = 0; i < dados.tecnologias.length; i += grupoTamanho) {
-        habilidadesEmGrupos.push(dados.tecnologias.slice(i, i + grupoTamanho));
+    // ALTERAÇÃO 2: Corrigido de 'dados.tecnologias' para 'dados.habilidades' para corresponder ao seu arquivo de dados.
+    for (let i = 0; i < dados.habilidades.length; i += grupoTamanho) {
+        habilidadesEmGrupos.push(dados.habilidades.slice(i, i + grupoTamanho));
     }
 
     useEffect(() => {
@@ -25,8 +27,8 @@ function S2_Habilidades({ isDark }) {
                 data-aos="fade-up"
                 data-aos-delay="100"
                 className="mb-6 relative inline-block text-lg text-[var(--color-primary)] font-semibold 
-                     after:absolute after:block after:bg-[var(--color-highlight)] after:content-[''] 
-                     after:rounded-full after:w-8 after:h-1 after:mt-1 after:left-0"
+                           after:absolute after:block after:bg-[var(--color-highlight)] after:content-[''] 
+                           after:rounded-full after:w-8 after:h-1 after:mt-1 after:left-0"
             >
                 Habilidades
             </h5>
